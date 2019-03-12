8d9aafe3e34842e4f51e5c6c5c22df8efa997b97
Kraramel <asri.med.amine@gmail.com>
2019-03-12 22:57:43 +0100

    adding comments

--- arrayClass.js
+++ arrayClass.js
@@ -1,19 +1,24 @@
 class MyArray {
+
+    // This the constructor of the class
     constructor(){
         this.length = 0;
         this.data = {};
     }
 
+    // This is the method to get the item of a specific index
     get(index){
         return this.data[index];
     }
 
+    // This method adds an item in the end of the array
     push(item){
         this.data[this.length] = item;
         this.length++;
         return this.length;
     }
 
+    // This method deletes the last item of an array
     pop(){
         const lastItem = this.data[this.length - 1];
         delete this.data[this.length - 1];
@@ -21,12 +26,14 @@ class MyArray {
         return lastItem;
     }
 
+    // This method deletes a specific item with his index
     delete(index){
         const item =this.data[index];
         this.shiftItems(index);
         return item;
     }
 
+    // This is a function that helps us to reorganize our array by shifting the items to the left and deleting the last
     shiftItems(index){
         for (let i = index; i < this.length -1; i++) {
             this.data[i] = this.data[i+1];
@@ -35,6 +42,7 @@ class MyArray {
         this.length--;
     }
 }
+// These are some tests that can help you understand the process
 
 const newArray = new MyArray();
 console.log(newArray);
